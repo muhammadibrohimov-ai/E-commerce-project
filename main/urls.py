5a0f8b2e4f76db507a8a2a0ce17528c933be0859
Muhammadjon Ibrohimov <miuceo@Muhammadjons-MacBook-Air.local>
2025-10-29 09:24:58 +0500

    added listings

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, ListingGridPageView, ListingLargePageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('listing-grid/', ListingGridPageView.as_view(), name='listing-grid'),
+    path('listing-large/', ListingLargePageView.as_view(), name='listing-large')
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,14 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
+
 class HomePageView(TemplateView):
     template_name = 'main/index.html'
-
+    
+    
+class ListingGridPageView(TemplateView):
+    template_name = 'main/listing-grid.html'
+    
+    
+class ListingLargePageView(TemplateView):
+    template_name = 'main/listing-large.html'
